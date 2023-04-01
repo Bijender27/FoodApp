68f7748870182ad0bef64afd25ab047b87beddea
Bijender <bijender@kenresarch.com>
2023-04-01 21:58:19 +0530

    FoopApp

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import classes from "./Header.module.css"
+import HeaderImg from "../../assets/meals.jpg"
+import HeaderCartButton from './HeaderCartButton';
+
+function Header() {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>FoodApp</h1>
+        <HeaderCartButton/>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={HeaderImg} alt="Header Image" />
+      </div>
+    </>
+  );
+}
+
+export default Header
